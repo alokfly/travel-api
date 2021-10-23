5a3777f6903580b4f8ae429162c39cbacd1af700
alok2110 <aloksaxena755@gmail.com>
2021-10-23 13:46:38 +0530

    new changes

--- routes/bestCollectionRoutes.js
+++
@@ -1,9 +0,0 @@
-const app = require("express");
-const router = app.Router();
-const {
-  viewBestCollection,
-} = require("../controllers/bestCollectionController");
-
-router.get("/viewBestCollection", viewBestCollection);
-
-module.exports = router;
